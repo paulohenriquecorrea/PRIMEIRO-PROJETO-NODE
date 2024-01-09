d0ef9794e98396ef6dbf35eae4eb84c5ceb4a9af
unknown <ph.sistemas.ti@gmail.com>
2024-01-09 17:28:25 -0400

    Finalizado 'Debugando NodeJs'

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,16 @@ const routes = Router();
 
 routes.get('/', (request, response) => response.json({message: 'Hello, World!'}));
 
+routes.post('/users', (request, response) => {
+	const {nome, email} = request.body;
+
+	const user = {
+		nome,
+		email,
+	};
+
+	response.json(user);
+});
+
 export default routes;
 
