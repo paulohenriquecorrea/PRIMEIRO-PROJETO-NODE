52741a906622ffa3d41b52d775edeb77fe954e73
unknown <ph.sistemas.ti@gmail.com>
2024-01-10 17:11:23 -0400

    Add validação de datas

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,19 +1,34 @@
 import {Router} from 'express';
 import {uuid} from 'uuidv4';
+import {startOfHour, parseISO, isEqual} from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+type Appointment = {
+	id: string;
+	provider: string;
+	date: Date;
+};
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.get('/', (request, response) => response.json({message: 'Hello, World!'}));
 
 appointmentsRouter.post('/', (request, response) => {
 	const {provider, date} = request.body;
 
+	const parsedDate = startOfHour(parseISO(date));
+
+	const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date));
+
+	if (findAppointmentInSameDate) {
+		return response.status(400).json({message: 'This appointment is already bookded'});
+	}
+
 	const appointment = {
 		id: uuid(),
 		provider,
-		date,
+		date: parsedDate,
 	};
 
 	appointments.push(appointment);
