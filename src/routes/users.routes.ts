30e2f27e21190a4b7bc31314acdcdee3305d8dde
unknown <ph.sistemas.ti@gmail.com>
2024-02-04 13:09:18 -0400

    Adicionado atualização de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,6 +6,8 @@ import CreateUserService from '../services/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
@@ -28,8 +30,21 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-	console.log(request.file);
-	return response.json({ok: true});
+	const user_id = request.user.id;
+	const avatarFilename = request.file.filename;
+	try {
+		const updateUserAvatarService = new UpdateUserAvatarService();
+
+		const user = await updateUserAvatarService.execute({
+			user_id, avatarFilename,
+		});
+
+		delete user.password;
+
+		return response.json(user);
+	} catch (error) {
+		return response.status(400).json({error: error.message});
+	}
 });
 
 export default usersRouter;
